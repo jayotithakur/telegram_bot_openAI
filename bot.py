c37f3f5c34140a06c31e970a3984edae9a049648
jayotithakur <jayotithakur10@gmail.com>
2023-08-13 21:39:09 -0400

    bot added

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telegram.ext
 from process import ask, append_interaction_to_chat_log
+from sklearn.metrics.pairwise import cosine_similarity
 import logging, os
 
 PORT = int(os.environ.get('PORT', '8443'))
@@ -9,6 +10,12 @@ with open('token.txt', 'r') as f:
 
 session = {}
 
+def evaluate_response(generated_response, actual_response):
+    # Calculate the cosine similarity between the generated and actual responses
+    embeddings = openai.Embedding.create([generated_response, actual_response])
+    embeddings = np.array(embeddings.embeddings)
+    cosine_sim = cosine_similarity([embeddings[0]], [embeddings[1]])[0][0]
+    return cosine_sim
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,14 +23,14 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 def start(update, context):
-    update.message.reply_text("Hello! Welcome to TriBot")
+    update.message.reply_text("Hello! Welcome to HealthGuardBot")
 
 def help(update, context):
     update.message.reply_text("""
     The Following commands are available:
-    /start -> Welcome to Trigan
+    /start -> Welcome to HealthGuard
     /help ->This Message
-    /about -> About TriBot
+    /about -> About  HealthGuardBot
     /contact -> Developer Info
     """)
 
@@ -32,7 +39,7 @@ def error(update, context):
 
 def about(update, context):
     update.message.reply_text("""
-            ExpdjBot is not just a chatbot. It's so much more than that. It's an AI-enabled customer service solution that answers your questions, responds to your tweets, and helps you find the products you're looking for. TriganBot has the power to save you time, increase your sales, and make your customer service operation more efficient.
+            HealthGuardBot is not just a chatbot. It's so much more than that. It's an AI-enabled customer service solution that answers your questions, responds to your tweets, and helps you find the products you're looking for. HealthGuardBot has the power to save you time, increase your sales, and make your customer service operation more efficient.
         """)
 
 def contact(update, context):
@@ -41,6 +48,15 @@ def contact(update, context):
 def handle_message(update, context):
     chat_log = session.get('chat_log')
     answer = ask(update.message.text, chat_log)
+
+    print(update.message.text)
+    print(chat_log)
+
+    # Evaluate the response using the cosine similarity metric
+    #evaluation_score = evaluate_response(answer, )
+    #print(f"Cosine Similarity Score: {evaluation_score}")
+
+
     session['chat_log'] = append_interaction_to_chat_log(update.message.text, answer, chat_log)
     update.message.reply_text(str(answer))
 
